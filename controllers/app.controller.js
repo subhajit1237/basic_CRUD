d478839ee51ea43d7f9e1b6d702b0012c50d7864
Subhajit Das <babai.dassubhajit@gmail.com>
2023-03-19 20:48:11 +0530

    update added

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -11,7 +11,7 @@ class AppController {
     async list(req, res) {
         try {
             let all_data = await App.find({isDeleted:false });
-            console.log(all_data);
+            // console.log(all_data);
             
             res.render('list', {
                 title: "List",
@@ -46,19 +46,22 @@ class AppController {
      */
     async insert(req, res){
         try{
-            console.log("here");
-            
-            let save_data =await App.create(req.body);
-            console.log(save_data);
-            
-            if(save_data && save_data.id){
-                console.log('Data save sucessfully');
+            let check_email = await App.find({email: req.body.email});
+            if(check_email.length != 0) {
+                console.log("Email exists");
                 res.redirect('/');
-            }else{
-                console.log('someting worng');
+            } else {
+                let save_data =await App.create(req.body);
+                // console.log(save_data);
                 
+                if(save_data && save_data.id){
+                    console.log('Data save sucessfully');
+                    res.redirect('/');
+                }else{
+                    console.log('someting worng');
+                    
+                }
             }
-            
             
         }catch(err){
             return err;
@@ -102,14 +105,9 @@ class AppController {
             if(delete_data){
             console.log("Data is Deleted");
             res.redirect('/')
-            
-
             }else{
-
             console.log("data is not deleted");
             res.redirect('/')
-            
-
             }
         
         }catch(err){
