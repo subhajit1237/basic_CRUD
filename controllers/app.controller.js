94d25a0ce59f3ef62a821b6311fe6d4b05e7e5c5
Subhajit Das <babai.dassubhajit@gmail.com>
2023-04-06 21:38:24 +0530

    Add files via upload
    controller updated

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,4 +1,6 @@
-const App =require('../models/app.model')
+const { sortBy } = require('underscore');
+const App =require('../models/app.model');
+const { user } = require('pg/lib/defaults');
 
 
 class AppController {
@@ -10,7 +12,7 @@ class AppController {
      */
     async list(req, res) {
         try {
-            let all_data = await App.find({isDeleted:false });
+            let all_data = await App.find({isDeleted:false }).sort({ createdAt: -1 });
             // console.log(all_data);
             
             res.render('list', {
@@ -31,7 +33,9 @@ class AppController {
     async create(req, res) {
         try {
             res.render('add', {
-                title: "Add"
+                title: "Add",
+                success: req.flash('success'),
+                error: req.flash('error')
                 
             })
         } catch (err) {
@@ -46,24 +50,38 @@ class AppController {
      */
     async insert(req, res){
         try{
+
+            console.log(req.body, 'req.body');
+            console.log(req.file, 'req.file');
+
+            // console.log('ehllo');
             let check_email = await App.find({email: req.body.email});
-            if(!_isEmpty(check_email)) {
-                console.log("Email already exists");
+            console.log(check_email);
+            if(!_.isEmpty(check_email)) {
+                // console.log("Email already exists");
+                req.flash('error','Email already exists!!!');
                 res.redirect('/');
             } else {
+                // console.log('hii');
                 let check_contact = await App.find({contact: req.body.contact })
                 // console.log(save_data);
-                if(!_isEmpty(check_contact)) {
-                    console.log("Contact already exists");
-                    res.redirect('/');
+                if(!_.isEmpty(check_contact)) {
+                    // console.log("Contact already exists");
+                    req.flash('error','Contact already exists!!!');
+                    return res.redirect('/');
                 } else {
+                    req.body.image = req.file.filename;
                     let save_data =await App.create(req.body);
-                    if(!_.isEmpty(save_data) && save_data.id){
-                        console.log('Data save sucessfully');
-                        res.redirect('/');
+                    // let save_data = true;
+                    console.log(save_data);
+                    if(!_.isEmpty(save_data)){
+                        // console.log('Data save sucessfully');
+                        req.flash('success','Data saved successfully!!!');
+                        return res.redirect('/');
                     }else{
-                        console.log('someting worng');
-                        
+                        // console.log('someting worng');
+                        req.flash('error','Data save failed!!!');
+                        return res.redirect('/');
                     }
                 }
             }
@@ -107,11 +125,13 @@ class AppController {
         try{
             let delete_data =await App.findByIdAndUpdate(req.params.id, {isDeleted : true});
             if(!_.isEmpty(delete_data)){
-            console.log("Data is Deleted");
-            res.redirect('/')
+            // console.log("Data is Deleted");
+            req.flash('success','Data is Deleted!!!');
+            return res.redirect('/')
             }else{
-            console.log("data is not deleted");
-            res.redirect('/')
+            // console.log("data is not deleted");
+            req.flash('error','data is not deleted!!!');
+            return res.redirect('/list')
             }
         
         }catch(err){
@@ -142,29 +162,40 @@ class AppController {
     async update(req, res) {
         try{
             let check_email = await App.find({ email: req.body.email, _id: { $ne: req.body.id } });
-            if(!_isEmpty(check_email)) {
-                console.log("Email already exists");
-                res.redirect('/');
+            if(!_.isEmpty(check_email)) {
+                // console.log("Email already exists");
+                req.flash('error','Email already exists!!!');
+                return res.redirect('/');
             } else {
                 let check_contact = await App.find({contact: req.body.contact, _id: { $ne: req.body.id } })
                 // console.log(save_data);
-                if(!_isEmpty(check_contact)) {
-                    console.log("Contact already exists");
-                    res.redirect('/');
+                if(!_.isEmpty(check_contact)) {
+                    // console.log("Contact already exists");
+                    req.flash('error','Contact already exists!!!');
+                    return res.redirect('/');
                 } else {
                     let updated_obj = {
                         name: req.body.name,
                         email: req.body.email,
                         contact: req.body.contact
                     }
+                    if (!_.isEmpty(req.file)) {
+                        req.body.image = req.file.filename;
+
+                        fs.unlinkSync(`./public/uploads/${user_data.image}`);
+                        update_obj.image = req.file.filename;
+
+                    }
                     let updated_data =await App.findByIdAndUpdate(req.body.id, updated_obj);
                     console.log(updated_data);
                     if(_.isEmpty(updated_data) && updated_data.id){
-                        console.log('Data Update sucessfully');
-                        res.redirect('/');
+                        // console.log('Data Update sucessfully');
+                        req.flash('success','Data Update successfully!!!');
+                        return res.redirect('/update');
                     }else{
-                        console.log('someting worng');
-                        res.render('/');
+                        // console.log('someting worng');
+                        req.flash('error','something went wrong!!!')
+                        return res.render('/');
                     }
                 }
             }
@@ -172,6 +203,26 @@ class AppController {
             return err;
         }
     }
+
+
+    /**
+     * @Method statusChange
+     * @Description To change the status
+     */
+        async statusChange(req, res) {
+            try {
+                let user_data = await App.findById(req.params.id);
+                console.log(user_data);
+                let updated_status = user_data.status === 'Active' ? 'Inactive' : 'Active';    
+                console.log(updated_status);
+                let update_data = await App.findByIdAndUpdate(req.params.id, {status: updated_status})
+                if(!_.isEmpty(user_data) && (update_data._id))
+                    res.redirect('/');
+            } catch (err) {
+                return err;
+            }
+        }
+    
 }
 
 
